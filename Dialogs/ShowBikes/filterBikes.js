3e75fe43185689871b93f7cdb5d4a3da1dc90274
Denner Santos <denner.santos@compasso.com.br>
2022-06-14 11:13:49 -0400

    Update filterBikes

--- Dialogs/ShowBikes/filterBikes.js
+++ Dialogs/ShowBikes/filterBikes.js
@@ -1,6 +1,7 @@
 const searchBikes = require('./searchBikes');
 module.exports = async (filter,value)=>{
     let bikes = await searchBikes();
+    if(!bikes){return false}
     return bikes.filter((bike)=>{
         if(bike[filter] == value){
             return bike;
