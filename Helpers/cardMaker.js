f657ea999ccf80181bb88ce3e28bd4b90cbe1876
Denner Santos <denner.santos@compasso.com.br>
2022-06-21 20:48:28 -0400

    Update cardMaker

--- Helpers/cardMaker.js
+++ Helpers/cardMaker.js
@@ -14,7 +14,7 @@ module.exports = {
         \nMarca: ${bike.brand}
         \nPreço:R$ ${bike.price}`,
         CardFactory.images([`${bike.image}`]),
-        ['Mais Informações sobre a bicicleta','Ver Proxima opção de bicicleta','Explorar outro filtro de pesquisa']
+        ['Mais Informacoes sobre a bicicleta','Ver Proxima opção de bicicleta','Explorar outro filtro de pesquisa']
     );
 },
     descriptionCard: (bike)=>{
