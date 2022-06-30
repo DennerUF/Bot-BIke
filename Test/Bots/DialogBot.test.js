99ab16c5fe1afea49a778afdd37acbeaf8c43158
Denner Santos <denner.santos@compasso.com.br>
2022-06-30 10:41:30 -0400

    Update test DialogBot

--- Test/Bots/DialogBot.test.js
+++ Test/Bots/DialogBot.test.js
@@ -4,17 +4,16 @@ const assert = require('assert');
 
 
 const sinon = require('sinon');
+let memoryStorage = new MemoryStorage();
+
+beforeEach(() => {
+    memoryStorage = new MemoryStorage();
+
+})
 
 describe('Teste DialogBot', () => {
 
-    // it('Caminho Certo', async () => {
-    //     sinon.stub(BikeRecognizer.prototype, 'executeLuisQuery').resolves({ luisResult: {} })
-    //     const dialog = {};
-    //     const memoryStorage = new MemoryStorage();
-    //     const sut = new DialogBot(new ConversationState(memoryStorage), new UserState(memoryStorage), dialog, console);
-    //     assert.deepStrictEqual(sut.onMessage({ activity: { type: 'message' } }), dataTest.onMessage)
 
-    // });
     it('Caminho errado - Nenhum parametro classe DialogBot ', async () => {
         assert.throws(() => {
             new DialogBot();
@@ -22,18 +21,18 @@ describe('Teste DialogBot', () => {
 
     });
     it('Caminho errado - Apenas conversationState como parametro na classe DialogBot ', async () => {
-        const memoryStorage = new MemoryStorage();
+
         assert.throws(() => {
             new DialogBot(new ConversationState(memoryStorage));
         })
     });
     it('Caminho errado - Apenas conversationState e userState como parametro na classe DialogBot ', async () => {
-        const memoryStorage = new MemoryStorage();
+
         assert.throws(() => {
-            new DialogBot(new ConversationState(memoryStorage),new UserState(memoryStorage));
+            new DialogBot(new ConversationState(memoryStorage), new UserState(memoryStorage));
         })
     });
-    
+
 
 
 
