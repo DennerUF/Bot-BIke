9dd1d9c5d0b90656c180d4b00bbc472eb74da9c8
Denner Santos <denner.santos@compasso.com.br>
2022-07-13 16:45:46 -0400

    Update documentation

--- Dialogs/ChangePurchaseData/message.js
+++ Dialogs/ChangePurchaseData/message.js
@@ -4,6 +4,11 @@ module.exports = {
         prompt: 'Todos os dados estão corretos ?',
     },
     messageConfirm: 'Para finalizarmos a compra, confirme os seus dados!',
+    /**
+     * Put the registration data in just one string
+     * @param {object} data registration data
+     * @returns {String} 
+     */
     purchaseData: (data) => {
         return `
         1. Cep: ${data.cep}
@@ -15,7 +20,6 @@ module.exports = {
         7. Nome: ${data.nome}
         8. CPF: ${data.cpf}
         9. Telefone: ${data.telefone}
-
     `
     },
     completedPurchase: 'Parabéns! Você acabou de finalizar a sua compra. Este é o número do seu pedido : 100',
@@ -23,6 +27,11 @@ module.exports = {
         prompt: 'Qual informação que deseja alterar ?',
         retryPrompt: `Não entendi qual dado deseja alterar. Para facilitar, você pode dizer o número da opção de 1 a 9`
     },
+    /**
+     * Add a variable to fixed text
+     * @param {String} data Name of the registration data to be changed  
+     * @returns {Prompt}
+     */
     messageChange: (data) => {
         return {
             prompt: `Me informe novamente ${data}`
